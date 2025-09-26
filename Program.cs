2d0d2b8bf957241bc5cb52d276b40f30bcaf3a47
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-09-26 00:29:47 -0300

    Utilizando o construtor para atribuir as informações das pessoas

--- Program.cs
+++ Program.cs
@@ -1,19 +1,10 @@
 ﻿using dio_dotnet_poo.Models;
 
-Pessoa p1 = new Pessoa();
-p1.Nome = "Marcos";
-p1.Sobrenome = "Marques";
-p1.Idade = 37;
+Pessoa p1 = new Pessoa("Marcos", "Marques", 37);
 
-Pessoa p2 = new Pessoa();
-p2.Nome = "Leonardo";
-p2.Sobrenome = "Buta";
-p2.Idade = 30;
+Pessoa p2 = new Pessoa("Leonardo", "Buta", 30);
 
-Pessoa p3 = new Pessoa();
-p3.Nome = "Eduardo";
-p3.Sobrenome = "Neves";
-p3.Idade = 22;
+Pessoa p3 = new Pessoa("Eduardo", "Neves", 22);
 
 Curso cursoDeIngles = new Curso();
 cursoDeIngles.Nome = "Inglês";
