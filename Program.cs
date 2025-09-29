644c91747c153d903b0627f0748c0c732099dbe1
Marcos Aleixo <maknytz@gmail.com>
2025-09-29 18:46:19 -0300

    Trabalhando com arquivos

--- Program.cs
+++ Program.cs
@@ -1,37 +1,9 @@
 ﻿using dio_dotnet_poo.Models;
 using System.Globalization;
 
-CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+string[] linhas = File.ReadAllLines("Files/arquivoLeitura.txt");
 
-decimal valorMonetario = 1582.40M;
-
-Console.WriteLine($"{valorMonetario:C}");
-Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
-
-double porcentagem = .3421;
-
-Console.WriteLine(porcentagem.ToString("P2"));
-
-DateTime data = DateTime.Now;
-Console.WriteLine(data);
-Console.WriteLine(data.ToShortDateString());
-Console.WriteLine(data.ToShortTimeString());
-
-
-
-
-
-
-
-Pessoa p1 = new Pessoa("Marcos", "Marques", 37);
-Pessoa p2 = new Pessoa("Leonardo", "Buta", 30);
-Pessoa p3 = new Pessoa("Eduardo", "Neves", 22);
-
-Curso cursoDeIngles = new Curso();
-cursoDeIngles.Nome = "Inglês";
-cursoDeIngles.Alunos = new List<Pessoa>();
-
-cursoDeIngles.AdicionarAluno(p1);
-cursoDeIngles.AdicionarAluno(p2);
-cursoDeIngles.AdicionarAluno(p3);
-cursoDeIngles.ListarAlunos();
+foreach (string linha in linhas)
+{
+    Console.WriteLine(linha);
+}
