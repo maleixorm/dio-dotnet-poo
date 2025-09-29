8a4b599a271c62305dcb3ad1b1bed42f4b91ed3f
Marcos Aleixo <maknytz@gmail.com>
2025-09-29 17:33:14 -0300

    Manipulando valores com o C#

---
+++ Files/ManipulacaoValores.cs
@@ -0,0 +1,15 @@
+// CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+
+// decimal valorMonetario = 1582.40M;
+
+// Console.WriteLine($"{valorMonetario:C}");
+// Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+
+// double porcentagem = .3421;
+
+// Console.WriteLine(porcentagem.ToString("P2"));
+
+// DateTime data = DateTime.Now;
+// Console.WriteLine(data);
+// Console.WriteLine(data.ToShortDateString());
+// Console.WriteLine(data.ToShortTimeString());
