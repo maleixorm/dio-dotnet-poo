9394e9292dd24de51b72fca90f0e42fcc29811b7
Marcos Aleixo <maknytz@gmail.com>
2025-09-29 17:32:58 -0300

    Trabalhando com propriedades, métodos e construtores

---
+++ Files/POO.cs
@@ -0,0 +1,12 @@
+// Pessoa p1 = new Pessoa("Marcos", "Marques", 37);
+// Pessoa p2 = new Pessoa("Leonardo", "Buta", 30);
+// Pessoa p3 = new Pessoa("Eduardo", "Neves", 22);
+
+// Curso cursoDeIngles = new Curso();
+// cursoDeIngles.Nome = "Inglês";
+// cursoDeIngles.Alunos = new List<Pessoa>();
+
+// cursoDeIngles.AdicionarAluno(p1);
+// cursoDeIngles.AdicionarAluno(p2);
+// cursoDeIngles.AdicionarAluno(p3);
+// cursoDeIngles.ListarAlunos();
