baed012bc5231f19664c38f3d3f614a2d8ec32de
Marcos Aleixo <maknytz@gmail.com>
2025-09-29 20:58:40 -0300

    Executando o método da classe ExemploExcessao

--- Program.cs
+++ Program.cs
@@ -1,25 +1,4 @@
 ﻿using dio_dotnet_poo.Models;
 using System.Globalization;
 
-
-
-try
-{
-    string[] linhas = File.ReadAllLines("Files/arquivoLeitura.txt");
-    foreach (string linha in linhas)
-    {
-        Console.WriteLine(linha);
-    }
-}
-catch (FileNotFoundException ex)
-{
-    Console.WriteLine("Ocorreu um erro na leitura do arquivo. Arquivo não encontrado! " + ex.Message);
-}
-catch (DirectoryNotFoundException ex)
-{
-    Console.WriteLine("Ocorreu um erro no diretório do arquivo. Diretório não encontrado! " + ex.Message);
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Ocorreu uma excessão genérica. {ex.Message}");
-}
+new ExemploExcecao().Metodo1();
