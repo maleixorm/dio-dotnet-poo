496341a7f9d6bda5dfb71a185de75bf761e1bb79
Marcos Aleixo <maknytz@gmail.com>
2025-09-29 20:36:51 -0300

    Criando exceções para os erros

--- Program.cs
+++ Program.cs
@@ -11,6 +11,14 @@ try
         Console.WriteLine(linha);
     }
 }
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine("Ocorreu um erro na leitura do arquivo. Arquivo não encontrado! " + ex.Message);
+}
+catch (DirectoryNotFoundException ex)
+{
+    Console.WriteLine("Ocorreu um erro no diretório do arquivo. Diretório não encontrado! " + ex.Message);
+}
 catch (Exception ex)
 {
     Console.WriteLine($"Ocorreu uma excessão genérica. {ex.Message}");
