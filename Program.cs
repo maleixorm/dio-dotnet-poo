c940bd51d1b37bca9ffbf8eecbef491049dbaa1f
Marcos Aleixo <maknytz@gmail.com>
2025-09-30 20:07:33 -0300

    Representando tuplas de diferentes maneiras

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 ﻿using dio_dotnet_poo.Models;
 using System.Globalization;
 
-(int, string, string, decimal) tupla = (1, "Marcos", "Marques", 1.67M);
+(int Id, string Nome, string Sobrenome, decimal Altura) tupla = (1, "Marcos", "Marques", 1.67M);
 
-Console.WriteLine($"ID: {tupla.Item1}");
-Console.WriteLine($"Nome: {tupla.Item2}");
-Console.WriteLine($"Sobrenome: {tupla.Item3}");
-Console.WriteLine($"Altura: {tupla.Item4}");
+ValueTuple<int, string, string, decimal> outroExemploTupla = (2, "Leonardo", "Buta", 1.80M);
+
+var outroExemploTuplaCreate = Tuple.Create(3, "João", "Neves", 1.74M);
+
+
+Console.WriteLine($"Id: {tupla.Id}");
+Console.WriteLine($"Nome: {tupla.Nome}");
+Console.WriteLine($"Sobrenome: {tupla.Sobrenome}");
+Console.WriteLine($"Altura: {tupla.Altura}");
