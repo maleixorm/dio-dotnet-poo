ddc7e43eebfe5c1e39c4851c19c76bad810c416d
Marcos Aleixo <maknytz@gmail.com>
2025-09-30 20:46:08 -0300

    Trabalhando com Tuplas

---
+++ Files/Tuplas.cs
@@ -0,0 +1,11 @@
+// (int Id, string Nome, string Sobrenome, decimal Altura) tupla = (1, "Marcos", "Marques", 1.67M);
+
+// ValueTuple<int, string, string, decimal> outroExemploTupla = (2, "Leonardo", "Buta", 1.80M);
+
+// var outroExemploTuplaCreate = Tuple.Create(3, "João", "Neves", 1.74M);
+
+
+// Console.WriteLine($"Id: {tupla.Id}");
+// Console.WriteLine($"Nome: {tupla.Nome}");
+// Console.WriteLine($"Sobrenome: {tupla.Sobrenome}");
+// Console.WriteLine($"Altura: {tupla.Altura}");
