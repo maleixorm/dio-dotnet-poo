f5f03408fb865bef4c271cbb22316c06be1f7792
Marcos Aleixo <maknytz@gmail.com>
2025-09-30 19:52:43 -0300

    Criando e manipulando um dictionary

---
+++ Files/Dictionary.cs
@@ -0,0 +1,19 @@
+// Dictionary<string, string> estados = new Dictionary<string, string>();
+
+// estados.Add("SP", "São Paulo");
+// estados.Add("MG", "Minas Gerais");
+// estados.Add("SC", "Santa Catarina");
+// estados.Add("BA", "Bahia");
+
+// foreach (var item in estados)
+// {
+//     Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+// }
+
+// estados.Remove("BA");
+// estados["SC"] = "Florianópolis";
+
+// foreach (var item in estados)
+// {
+//     Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+// }
