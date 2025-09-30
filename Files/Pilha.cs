cc0cab222b02b368b9f8475015f044acd754554b
Marcos Aleixo <maknytz@gmail.com>
2025-09-29 21:36:03 -0300

    Trabalhando com Pilhas no C#

---
+++ Files/Pilha.cs
@@ -0,0 +1,18 @@
+// Stack<int> pilha = new Stack<int>();
+
+// pilha.Push(2);
+// pilha.Push(4);
+// pilha.Push(6);
+// pilha.Push(8);
+
+// foreach (int item in pilha)
+// {
+//     Console.WriteLine(item);
+// }
+
+// pilha.Pop();
+
+// foreach (int item in pilha)
+// {
+//     Console.WriteLine(item);
+// }
