711d2258f35a85593ed8527ce8fa64ced6cf5e15
Marcos Aleixo <maknytz@gmail.com>
2025-09-29 21:35:45 -0300

    Trabalhando com filas no C#

---
+++ Files/Fila.cs
@@ -0,0 +1,18 @@
+// Queue<int> fila = new Queue<int>();
+
+// fila.Enqueue(2);
+// fila.Enqueue(4);
+// fila.Enqueue(6);
+// fila.Enqueue(8);
+
+// foreach (int item in fila)
+// {
+//     Console.WriteLine(item);
+// }
+
+// fila.Dequeue();
+
+// foreach (int item in fila)
+// {
+//     Console.WriteLine(item);
+// }
