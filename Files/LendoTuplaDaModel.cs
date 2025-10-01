a082e588302c561fdf8a70963fafcbe4a70fa291
Marcos Aleixo <maknytz@gmail.com>
2025-09-30 21:18:18 -0300

    Lendo a tupla da classe LeituraArquivo

---
+++ Files/LendoTuplaDaModel.cs
@@ -0,0 +1,16 @@
+// LeituraArquivo arquivo = new LeituraArquivo();
+
+// var (sucesso, linhas, quantidadeLinhas) = arquivo.LerArquivo("Files/arquivoLeitura.txt");
+
+// if (sucesso)
+// {
+//     Console.WriteLine("Quantidade de linhas no arquivo: " + quantidadeLinhas);
+//     foreach (string linha in linhas)
+//     {
+//         Console.WriteLine(linha);
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Não foi possível ler o arquivo!");
+// }
