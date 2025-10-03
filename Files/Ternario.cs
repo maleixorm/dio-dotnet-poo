a2e9b3c31c8ce278894271fb8896906107e2bd46
Marcos Aleixo <maknytz@gmail.com>
2025-10-03 16:55:14 -0300

    Utilizando o If Ternário no C#

---
+++ Files/Ternario.cs
@@ -0,0 +1,15 @@
+// int numero = 20;
+// bool ehPar = false;
+
+// ehPar = numero % 2 == 0;
+// Console.WriteLine($"O numero {numero} é: " + (ehPar ? "par" : "ímpar") + ".");
+
+
+// if (numero % 2 == 0)
+// {
+//     Console.WriteLine($"O numero {numero} é par!");
+// }
+// else
+// {
+//     Console.WriteLine($"O numero {numero} é ímpar!");
+// }
