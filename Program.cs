dfe7e0fe50e61dfb20556ce738f35bd7bf826a14
Marcos Aleixo <maknytz@gmail.com>
2025-10-03 17:20:30 -0300

    Executando os métodos da classe ContaCorrente

--- Program.cs
+++ Program.cs
@@ -4,3 +4,8 @@ using System.Globalization;
 Pessoa p1 = new Pessoa("Marcos", "Marques", 37);
 
 p1.Apresentar();
+
+ContaCorrente c1 = new ContaCorrente(123, 1000);
+c1.ExibirSaldo();
+c1.Sacar(300);
+c1.ExibirSaldo();
