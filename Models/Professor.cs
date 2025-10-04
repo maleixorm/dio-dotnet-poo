b3b8bba1f7fb916129d3d84854d93355866e1850
Marcos Aleixo <maknytz@gmail.com>
2025-10-04 13:50:23 -0300

    Criando um método Apresentar polimorfo

--- Models/Professor.cs
+++ Models/Professor.cs
@@ -8,5 +8,10 @@ namespace dio_dotnet_poo.Models
     public class Professor : Pessoa
     {
         public decimal Salario { get; set; }
+
+        public override void Apresentar()
+        {
+            Console.WriteLine($"Eu sou o {Nome} e meu salário é {Salario.ToString("C")}");
+        }
     }
 }
