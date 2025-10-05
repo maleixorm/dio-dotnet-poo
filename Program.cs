98fb3b860628b49566a04b05285eb9d7fad4b5d3
Marcos Aleixo <maknytz@gmail.com>
2025-10-05 19:37:18 -0300

    Implementando as classes Pessoa e Aluno

--- Program.cs
+++ Program.cs
@@ -4,3 +4,7 @@ using System.Globalization;
 Corrente c1 = new Corrente();
 c1.Creditar(500);
 c1.ExibirSaldo();
+
+Pessoa p1 = new Pessoa("Marcos");
+Aluno a1 = new Aluno("Eduardo");
+a1.Apresentar();
