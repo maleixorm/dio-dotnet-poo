f15cb2716c752cf17b10b382779d770ce14eeaa5
Marcos Aleixo <maknytz@gmail.com>
2025-10-05 19:10:49 -0300

    Criando e executando métodos de uma conta corrente

--- Program.cs
+++ Program.cs
@@ -1,24 +1,6 @@
 ﻿using dio_dotnet_poo.Models;
 using System.Globalization;
 
-Pessoa p1 = new Pessoa("Marcos", "Marques", 37);
-p1.Apresentar();
-
-ContaCorrente c1 = new ContaCorrente(123, 1000);
+Corrente c1 = new Corrente();
+c1.Creditar(500);
 c1.ExibirSaldo();
-c1.Sacar(300);
-c1.ExibirSaldo();
-
-Aluno a1 = new Aluno();
-a1.Nome = "Leonardo";
-a1.Sobrenome = "Buta";
-a1.Idade = 30;
-a1.Nota = 9.35;
-a1.Apresentar();
-
-Professor prof1 = new Professor();
-prof1.Nome = "Eduardo";
-prof1.Sobrenome = "Menezes";
-prof1.Idade = 48;
-prof1.Salario = 3500M;
-prof1.Apresentar();
